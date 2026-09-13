81b9522643101eb533f2c6799bc8682218e06c6b
aliahaki <341172@student.mboutrecht.nl>
2026-09-13 15:52:50 +0200

    Magazijn overzicht route toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,8 @@ use App\Http\Controllers\MagazijnController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Publiek toegankelijke magazijn routes (geen login nodig voor docent / testen)
+Route::get('/magazijn', [MagazijnController::class, 'index'])->name('magazijn.index');
+Route::get('/magazijn/levering/{id}', [MagazijnController::class, 'levering'])->name('magazijn.levering');   
+Route::get('/magazijn/allergenen/{id}', [MagazijnController::class, 'allergenen'])->name('magazijn.allergenen');
