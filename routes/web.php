e468317dac87b3c4abc89d7c99be2b792ec74e1a
aliahaki <341172@student.mboutrecht.nl>
2026-09-13 15:51:27 +0200

    Controllers en Route imports toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -7,36 +7,3 @@ use App\Http\Controllers\KlantController;
 use App\Http\Controllers\MagazijnmedewerkerController;
 use App\Http\Controllers\MagazijnController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Publiek toegankelijke magazijn routes (geen login nodig voor docent / testen)
-Route::get('/magazijn', [MagazijnController::class, 'index'])->name('magazijn.index');
-Route::get('/magazijn/levering/{id}', [MagazijnController::class, 'levering'])->name('magazijn.levering');   
-Route::get('/magazijn/allergenen/{id}', [MagazijnController::class, 'allergenen'])->name('magazijn.allergenen');
-
-Route::get('/admin', [AdminController::class, 'index'])
-    ->name('admin.index')
-    ->middleware(['auth', 'role:admin']);
-
-// Zowel klant als admin krijgen toegang tot de klant homepage:
-Route::get('/klant', [KlantController::class, 'index'])
-    ->name('klant.index')
-    ->middleware(['auth', 'role:klant,admin']);    
-
-Route::get('/magazijnmedewerker', [MagazijnmedewerkerController::class, 'index'])
-    ->name('magazijnmedewerker.index')
-    ->middleware(['auth', 'role:magazijnmedewerker']);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
