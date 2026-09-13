768d1e20d962556cf780b8f776cbb12909798596
aliahaki <341172@student.mboutrecht.nl>
2026-09-13 16:27:59 +0200

    Profile en authenticatie routes toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,20 @@ Route::get('/admin', [AdminController::class, 'index'])
 // Zowel klant als admin krijgen toegang tot de klant homepage:
 Route::get('/klant', [KlantController::class, 'index'])
     ->name('klant.index')
-    ->middleware(['auth', 'role:klant,admin']); 
-    
+    ->middleware(['auth', 'role:klant,admin']);    
+
 Route::get('/magazijnmedewerker', [MagazijnmedewerkerController::class, 'index'])
     ->name('magazijnmedewerker.index')
     ->middleware(['auth', 'role:magazijnmedewerker']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');    
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
