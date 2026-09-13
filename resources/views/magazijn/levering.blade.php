4c19924ab4f9a0c25059799f37acd905e82154dc
aliahaki <341172@student.mboutrecht.nl>
2026-09-13 16:44:03 +0200

    Terug knop toegevoegd aan levering informatie view

--- resources/views/magazijn/levering.blade.php
+++ resources/views/magazijn/levering.blade.php
@@ -57,6 +57,11 @@
                     </script>
                 @endif
 
+                <div class="mt-4">
+                    <a href="{{ route('magazijn.index') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                           Terug naar magazijn
+                   </a>
+                </div>
             </div>
         </div>
     </div>
